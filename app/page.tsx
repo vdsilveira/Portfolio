511917a9f9b341ddfb163564b233fbc30e0f2195
vdsilveira <122502607+vdsilveira@users.noreply.github.com>
2024-06-04 15:28:13 -0300

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
 
       <div
         className="flex items-center w-full h-full bg-cover bg-center"
-        style={{ backgroundImage: "url('/main-bg.webp')" }}
+        style={{ backgroundImage: `url(${require("/public/main-bg.webp")})` }}
       >
         <div className="pl-20 md:pl-40 pb-56 md:pb-20 flex flex-col gap-5 z-[10] max-w-[750px]">
           <h1 className="text-[50px] text-white font-semibold">
