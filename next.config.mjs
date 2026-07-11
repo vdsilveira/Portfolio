4e927265bc2be0375492d807ffe9675263fcaf24
viniciusd <viniciusd@cpqd.com.br>
2026-07-11 15:56:22 -0300

    fix: add conditional assetPrefix to fix broken images on GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  assetPrefix: process.env.GITHUB_ACTIONS ? "/Portfolio" : "",
   images: {
     unoptimized: true,
   },
